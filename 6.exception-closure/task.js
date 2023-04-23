6d0676f4eadc5a9a345c1d5aa7200a2ee1d73741
NeuroK-hub <il_kuzmin@mail.ru>
2023-04-23 18:53:19 +0300

    task 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(a) {
+	if (Number.isNaN(Number.parseFloat(a))) {
+		throw new Error('Невалидное значение')
+	}
+	return Number.parseFloat(a)
+}
+
+function validateCount(a) {
+	try {
+		return parseCount(a)
+	} catch (Error) {
+		return Error
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if ((a + b) < c || (a + c) < b || (c + b) < a) {
+			throw new Error('Треугольник с такими сторонами не существует')
+		} else {
+			this.a = a
+			this.b = b
+			this.c = c
+		}
+
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c
+	}
+
+	get area() {
+		let p = this.perimeter * 0.5
+		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c)
+	} catch (ettor) {
+		return {
+			get area() {
+				return ('Ошибка! Треугольник не существует')
+			},
+			get perimeter() {
+				return ('Ошибка! Треугольник не существует')
+			}
+		}
+	}
+}
